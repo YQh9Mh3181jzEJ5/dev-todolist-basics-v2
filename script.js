39b65f17c6f0b965259042c2d3535cef3434c69b
u6^6P%4*&a0n@6o <mfghz487k44o6u6ekgub@gmail.com>
2023-03-21 14:20:58 +0900

    基本機能は完了

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    add(todo);
+  });
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(input.value);
+  add();
 });
+
+function add(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+
+    if (todo && todo.completed) {
+      li.classList.add("completed");
+    }
+
+    li.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
+    li.addEventListener("click", () => {
+      li.classList.toggle("completed");
+            saveData();
+    });
+
+    ul.appendChild(li);
+    input.value = "";
+    saveData();
+  }
+}
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach((list) => {
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("completed"),
+    };
+    todos.push(todo);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
